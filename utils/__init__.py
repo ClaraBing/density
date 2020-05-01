6bd96b15af535dd6bddf50c0619ff86d6ba5cb66
ClaraBing <bingbinl@cs.cmu.edu>
2020-05-01 10:13:16 -0400

    reorganize & add in utils for flows

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
-from util.array_util import mean_dim
-from util.optim_util import bits_per_dim, clip_grad_norm, NLLLoss
-from util.shell_util import AverageMeter
-from util.rbig_util import *
+from utils.array_util import mean_dim
+from utils.optim_util import bits_per_dim, clip_grad_norm, NLLLoss
+from utils.shell_util import AverageMeter
+from utils.rbig_util import *
+from utils.flows_util import *
