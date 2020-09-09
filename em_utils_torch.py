414c15aa62feed4be7e6fe346a6b3695b556f666
ClaraBing <bingbinl@cs.cmu.edu>
2020-09-09 10:25:05 -0400

    add a loop count to avoid infinite loop

--- em_utils_torch.py
+++ em_utils_torch.py
@@ -204,7 +204,8 @@ def EM(X, K, gamma, A, pi, mu, sigma_sqr, threshold=5e-5, A_mode='GA',
           flag = True
           gnorm = torch.norm(grad)
           n_iter = 0
-          while flag:
+          ITER_LIM = 10
+          while flag and n_iter < ITER_LIM:
             n_iter += 1
             Ap = A + t * grad
             _, ss, _ = torch.svd(Ap)
