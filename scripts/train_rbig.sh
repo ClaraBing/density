1c68ac8854ec1d12d519361500bdcb859938b39a
ClaraBing <bingbinl@cs.cmu.edu>
2020-10-30 14:34:41 -0400

    script for RBIG

--- scripts/train_rbig.sh
+++ scripts/train_rbig.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
+rot_type='ICA'
+
 # dataset='GaussianLine'
 # dataset='GaussianMixture'
 # dataset='uniform'
 # dataset='GAS8'
 # dataset='miniboone'
-dataset='GaussianMixture'
+# dataset='GaussianMixture'
+dataset='MNISTtab'
 dlen=50000
+bt_test=100
 
-dataset='GM'
-n_layer=10
+# dataset='GM'
 
 # save_suffix='_data200k_run1'
-save_suffix='_btVal100_checkKL'
+# save_suffix='_btVal100_randProj'
+# save_suffix='_btVal'$bt_test'_checkCov_proj10'
+save_suffix='_btVal'$bt_test
 
-for n_layer in 5 6 8 10 12
+for run in 1
 do
-CUDA_VISIBLE_DEVICES=0 python -W ignore rbig.py \
+save_suffix='_btVal'$bt_test'_run'$run
+for n_layer in 20
+do
+CUDA_VISIBLE_DEVICES=1 python -W ignore rbig.py \
   --model='rbig' \
   --dataset=$dataset \
   --use-val=1 \
   --bt=50000 \
-  --bt-test=100 \
-  --rotation-type='ICA' \
+  --bt-test=$bt_test \
+  --rotation-type=$rot_type \
   --n-layer=$n_layer \
   --save-suffix=$save_suffix \
   --dlen=$dlen
+done
 done
 
