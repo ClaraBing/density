685a3a7b3f3664c97886d057e9973d63a1a68d5f
ClaraBing <bingbinl@cs.cmu.edu>
2020-11-01 20:29:51 -0500

    update: remove lib

--- scripts/em_fit_miniboone.sh
+++ scripts/em_fit_miniboone.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-gpu_id=0
+gpu_id=3
 
-# mode='GA'
+mode='GA'
 # mode='torchGA'
 # mode='torchAll'
-mode='None'
-# mode='CF'
-gamma=0.003
-gamma_min=3e-3
+mode='ICA'
+gamma=0
+gamma_min=0
+# gamma=0.001
+# gamma_min=1e-3
 # gamma=-1
 # gamma_min=-1
 
@@ -17,43 +18,24 @@ check_obj=0
 
 data='miniboone'
 
-save_token='_debug_newICA'
+save_token='_ICA'
 
-for iter in 1
+for iter in 1 2
 do
-if [ $iter -eq 1 ]; then
-  K=20
-  n_em=30
-  n_gd=30
-  n_pts=27000
-fi
-if [ $iter -eq 2 ]; then
-  K=20
-  n_em=40
-  n_gd=20
-  n_pts=27000
-fi
-if [ $iter -eq 3 ]; then
-  K=40
-  n_em=30
-  n_gd=30
-  n_pts=14000
-fi
-if [ $iter -eq 4 ]; then
-  K=20
-  n_em=30
-  n_gd=20
-  n_pts=14000
-fi
+save_token=$save_token'_run'$iter
+for K in 40
+do
+for n_em in 40
+do
 
 if [ $mode = 'ICA' ]; then
   n_pts=0
+  n_gd=0
 fi
 
 CUDA_VISIBLE_DEVICES=$gpu_id python em_fit.py \
   --data=$data \
   --mode=$mode \
-  --lib='torch' \
   --gamma=$gamma \
   --gamma-min=$gamma_min \
   --K=$K \
@@ -64,4 +46,6 @@ CUDA_VISIBLE_DEVICES=$gpu_id python em_fit.py \
   --save-token=$save_token \
   --time=$TIME \
   --check-obj=$check_obj
+done
+done
 done
