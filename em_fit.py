6764cdbe5f6d689ea7af36f18919d120dfd11828
liruohan1436311 <u3523358@connect.hku.hk>
2021-01-20 23:16:48 -0500

    Add minibatch training.

--- em_fit.py
+++ em_fit.py
@@ -54,6 +54,7 @@ parser.add_argument('--var-A-mode', type=str, default='GD', choices=['GD', 'fixe
 parser.add_argument('--var-pos-type', type=str, default='smoothL1')
 parser.add_argument('--var-num-hidden-nodes', type=int, default=10)
 parser.add_argument('--var-num-layers', type=int, default=1)
+parser.add_argument('--var-batch-size', type=int, default=10000)
 # saving & logging
 parser.add_argument('--overwrite', type=int, default=0,
                    help="Whether to overwrite an existing directory.")
